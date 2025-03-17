02e5885b4d70ca584cd282d721ddce17c6464b87
Eugene Murray <eugene.murray@gmail.com>
2025-03-17 17:17:46 +0000

    update to log values

--- TrendBot/TrendBot.cs
+++ TrendBot/TrendBot.cs
@@ -9,6 +9,7 @@
 
 using cAlgo.API;
 using cAlgo.API.Indicators;
+using System.Diagnostics;
 
 namespace cAlgo.Robots
 {
@@ -20,6 +21,9 @@ namespace cAlgo.Robots
         private MovingAverage _fastMa;
 
         private MovingAverage _slowMa;
+
+        [Parameter(Group = "Moving Average")]
+        public DataSeries SourceSeries { get; set; }
 
         [Parameter("Source", Group = "Fast MA")]
         public DataSeries FastMaSource { get; set; }
@@ -51,6 +55,9 @@ namespace cAlgo.Robots
         [Parameter("Label", DefaultValue = "TrendBot", Group = "Trade")]
         public string Label { get; set; }
 
+        private MovingAverage _200Ma;
+        private AverageDirectionalMovementIndexRating _averageDirectionalMovementIndexRating;
+
         public Position[] BotPositions
         {
             get
@@ -61,45 +68,107 @@ namespace cAlgo.Robots
 
         protected override void OnStart()
         {
-            Print("OnStart");
-            
+            Print("## Algorithm started at {0}", Server.Time);
+
             _volumeInUnits = Symbol.QuantityToVolumeInUnits(VolumeInLots);
 
             _fastMa = Indicators.MovingAverage(FastMaSource, FastMaPeriod, FastMaType);
             _slowMa = Indicators.MovingAverage(SlowMaSource, SlowMaPeriod, SlowMaType);
+            _200Ma = Indicators.MovingAverage(SourceSeries, 200, MovingAverageType.Exponential);
+            _averageDirectionalMovementIndexRating = AverageDirectionalMovementIndexRating(20);
+
+            
 
             _fastMa.Result.Line.Color = Color.White;
             _slowMa.Result.Line.Color = Color.White;
+            _200Ma.Result.Line.Color = Color.White;
+            _200Ma.Result.Line.Thickness = 3;
         }
 
         protected override void OnBarClosed()
         {
-            Print("OnBarClosed");
-            
             if (_fastMa.Result.HasCrossedAbove(_slowMa.Result, 0))
             {
+
+                Print("#########");
+                Print("IsMaTrending: {0}", IsMaTrending());
+                IsAdxTrending();
+                Print("200 MA IsRising: {0}", _200Ma.Result.IsRising());
+                Print("Open New Position: BUY");
+                Print("#########");
+
                 ClosePositions(TradeType.Sell);
 
                 ExecuteMarketOrder(TradeType.Buy, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
             }
             else if (_fastMa.Result.HasCrossedBelow(_slowMa.Result, 0))
             {
+                Print("#########");
+                Print("IsMaTrending: {0}", IsMaTrending());
+                IsAdxTrending();
+                Print("200 MA IsRising: {0}", _200Ma.Result.IsRising());
+                Print("Open New Position: SELL");
+                Print("#########");
+
                 ClosePositions(TradeType.Buy);
 
                 ExecuteMarketOrder(TradeType.Sell, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
             }
         }
 
+
         private void ClosePositions(TradeType tradeType)
         {
-            Print("ClosePositions");
-            
+            Print("## ClosePositions");
+
             foreach (var position in BotPositions)
             {
                 if (position.TradeType != tradeType) continue;
 
                 ClosePosition(position);
             }
+        }
+
+        private bool IsMaTrending()
+        {
+            return (_slowMa.Result.IsRising() && _fastMa.Result.IsRising() && _200Ma.Result.IsRising())
+                   || (_slowMa.Result.IsFalling() && _fastMa.Result.IsFalling() && _200Ma.Result.IsFalling());
+
+        }
+
+        private bool IsAdxTrending()
+        {
+            //Print("ADXR Last 0: {0}", _averageDirectionalMovementIndexRating.ADXR.Last(0));
+            //Print("ADXR Last 1: {0}", _averageDirectionalMovementIndexRating.ADXR.Last(1));
+            //Print("ADX Last 0: {0}", _averageDirectionalMovementIndexRating.ADXR.Last(0));
+            //Print("ADX Last 1: {0}", _averageDirectionalMovementIndexRating.ADX.Last(1));
+            if (_averageDirectionalMovementIndexRating.ADXR.Last(0) < 25 && (_200Ma.Result.IsRising() || _200Ma.Result.IsFalling()))
+            {
+                Print("ADX IsTrending: False");
+                return false;
+            }
+            Print("ADX IsTrending: True");
+            return true;
+        }
+
+        private ExponentialMovingAverage ExponentialMovingAverage(TimeFrame timeFrame, int period)
+        {
+            return Indicators.ExponentialMovingAverage(MarketData.GetBars(timeFrame).ClosePrices, period);
+        }
+
+        private AverageDirectionalMovementIndexRating AverageDirectionalMovementIndexRating(int period)
+        {
+            return Indicators.AverageDirectionalMovementIndexRating(period);
+        }
+
+        private void DebugBot()
+        {
+            var result = Debugger.Launch();
+        }
+
+        protected override void OnStop()
+        {
+            Print("Algorithm stopped at {0}", Server.Time);
         }
     }
 }
